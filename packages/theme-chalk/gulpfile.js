88135db81ed09a83f7867e79ee4f52142344aaa4
zhenyu.yu <zhenyu.yu@pinlandata.com>
2022-12-02 17:44:07 +0800

    elementui BEM风格实现

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -1,10 +1,10 @@
 'use strict';
-
+// gulp 配置文件
 const { series, src, dest } = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
-
+// 将 scss 编译成 css 并压缩，最后输出到 ./lib 目录下
 function compile() {
   return src('./src/*.scss')
     .pipe(sass.sync())
@@ -15,7 +15,7 @@ function compile() {
     .pipe(cssmin())
     .pipe(dest('./lib'));
 }
-
+// 拷贝 ./src/fonts 到 ./lib/fonts
 function copyfont() {
   return src('./src/fonts/**')
     .pipe(cssmin())
